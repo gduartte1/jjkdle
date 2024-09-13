31c7305ec91e89050a29b0c4c37a9b9904476933
RaicarvalhoRCO <rai_sp16@outlook.com>
2024-09-13 10:51:28 -0300

    Countdown 24h

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CharactersService } from '../characters.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -13,18 +14,70 @@ export class MainComponent implements OnInit {
   guessedCharacters: any[] = [];
   message: string = '';
   validCharacters: string[] = [];
-  searchResults: any[] = [];  // Altere para um array de objetos contendo nome e foto
+  searchResults: any[] = [];
+  timeRemaining: string = '';
 
   constructor(private charactersService: CharactersService) { }
 
   ngOnInit(): void {
-    this.loadRandomCharacter();
+    this.checkDailyCharacter();
     this.loadvalidCharacters();
+    this.startCountdown();
+  }
+  
+
+  checkDailyCharacter() {
+    const lastGeneratedDate = localStorage.getItem('lastGeneratedDate');
+    const now = new Date();
+
+    // Define o horário fixo para gerar o novo personagem (por exemplo, meia-noite)
+    const targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0);
+
+    if (lastGeneratedDate) {
+      const lastGenerated = new Date(lastGeneratedDate);
+      
+      // Verifica se o dia atual é diferente do dia do último personagem gerado
+      if (now.getDate() !== lastGenerated.getDate() || now.getTime() <= targetTime.getTime()) {
+        this.loadRandomCharacter();
+      } else {
+        // Carrega o personagem atual salvo
+        const savedChampion = localStorage.getItem('currentChampion');
+        if (savedChampion) {
+          this.currentCharacter = JSON.parse(savedChampion);
+        }
+      }
+    } else {
+      this.loadRandomCharacter(); // Gera o primeiro personagem se nunca foi gerado
+    }
+  }
+
+  getTimeRemaining(): string {
+    const now = new Date();
+    const nextReset = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0); // Próxima meia-noite
+    const diffInMs = nextReset.getTime() - now.getTime();
+
+    const hours = Math.floor((diffInMs / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diffInMs / (1000 * 60)) % 60);
+    const seconds = Math.floor((diffInMs / 1000) % 60);
+    return `${this.formatTime(hours)}:${this.formatTime(minutes)}:${this.formatTime(seconds)}`;
+  }
+
+  formatTime(unit: number): string {
+    return unit < 10 ? '0' + unit : unit.toString();
+  }
+
+  startCountdown() {
+    interval(1000).subscribe(() => {
+      this.timeRemaining = this.getTimeRemaining();
+    });
   }
 
   loadRandomCharacter() {
     const character = this.charactersService.getCharacters();
     this.currentCharacter = character[Math.floor(Math.random() * character.length)];
+    localStorage.setItem('currentChampion', JSON.stringify(this.currentCharacter));
+    const now = new Date();
+    localStorage.setItem('lastGeneratedDate', now.toISOString());
   }
 
   loadvalidCharacters() {
